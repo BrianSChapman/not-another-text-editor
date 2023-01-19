3d72bb82da6a67306451be5c2f1d67e8cac29509
Brian Chapman <chapman.brian86@gmail.com>
2023-01-19 15:25:05 -0500

    required and wrote out all plugin options for webpack

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,62 +1,79 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const WebpackPwaManifest = require('webpack-pwa-manifest');
-const path = require('path');
-const { InjectManifest } = require('workbox-webpack-plugin');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const WebpackPwaManifest = require("webpack-pwa-manifest");
+const path = require("path");
+const { InjectManifest } = require("workbox-webpack-plugin");
 
 // TODO: Add and configure workbox plugins for a service worker and manifest file.
 // TODO: Add CSS loaders and babel to webpack.
 
 module.exports = () => {
   return {
-    mode: 'development',
+    mode: "development",
     entry: {
-      main: './src/js/index.js',
-      install: './src/js/install.js'
+      main: "./src/js/index.js",
+      install: "./src/js/install.js",
     },
     output: {
-      filename: '[name].bundle.js',
-      path: path.resolve(__dirname, 'dist'),
+      filename: "[name].bundle.js",
+      path: path.resolve(__dirname, "dist"),
     },
     plugins: [
       new HtmlWebpackPlugin({
-        template: './index.html'
+        template: "./index.html",
       }),
       new InjectManifest({
-        swSrc: './src/src-sw.js',
-        swDest: 'service-worker.js',
+        swSrc: "./src/src-sw.js",
+        swDest: "service-worker.js",
       }),
       new WebpackPwaManifest({
-
-        "short_name": 'J.A.T.E.',
-        "name": 'Just Another Text Editor',
-        "icons": [
+        fingerprints: false,
+        inject: true,
+        short_name: "J.A.T.E.",
+        name: "Just Another Text Editor",
+        publicPath: "/",
+        start_url: "/",
+        background_color: "#272822",
+        display: "standalone",
+        theme_color: "#31a9e1",
+        description: "Jot down code snippets with ease!",
+        icons: [
           {
-            'src': '/client/favicon.ico',
-            'type': 'image/icon',
-            'sizes': '192x192',
-            'purpose': 'any maskable'
+            src: path.resolve("/client/favicon.ico"),
+            type: "image/icon",
+            sizes: 96,
+            purpose: "any maskable",
           },
           {
-            'src': '/client/src/images/logo.png',
-            'type': 'image/png',
-            'sizes': '512x512',
-            'purpose': 'any maskable'
-          }
+            src: path.resolve("/client/src/images/logo.png"),
+            type: "image/png",
+            sizes: [96, 128, 256, 384, 512],
+            destination: path.join("assets", "icons"),
+            purpose: "any maskable",
+          },
         ],
-        'id': '/',
-        'start_url': '/',
-        'background_color': '#272822',
-        'orientation': 'portrait',
-        'display': 'standalone',
-        'theme_color': '#31a9e1',
-        'description': 'Jot down code snippets with ease!'
-      })
-
+      }),
     ],
 
     module: {
       rules: [
-        
+        {
+          test: /\.css$/i,
+          use: ["style-loader", "css-loader"],
+        },
+        {
+          test: /\.m?js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: "babel-loader",
+            options: {
+              presets: ["@babel/preset-env"],
+              plugins: [
+                "@babel/plugin-proposal-object-rest-spread",
+                "@babel-transform-runtime",
+              ],
+            },
+          },
+        },
       ],
     },
   };
